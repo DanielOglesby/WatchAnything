10bf6f908d8fa706969604c89335548c52542a37
Daniel Oglesby <dgo4man1@gmail.com>
2023-06-24 16:46:37 -0500

    feat: initial page skeleton

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,60 @@
-<h1>Hello, welcome to WatchAnything!</h1>
+<header>
+    <div class="header-text">
+    <h1>Welcome to WatchAnything!</h1>
+    <h2>The place to watch anything with friends!</h2>
+</div>
+<div class="header-links">
+    <p><a href="/about">About</a></p>
+    </div>
+</header>
+
+<div class="center">
+    Middle div
+</div>
+
+<footer>
+    <p>Created by <a href="https://github.com/DanielOglesby" target="_blank">Daniel Oglesby</a></p>
+</footer>
+
+
+<style>
+    
+    .center{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        background-color: black;
+    }
+
+    .header-text{
+        display: flex;
+        flex-direction: row;
+        justify-content: left;
+        align-items: center;
+        height: 100px;
+        background-color: red;
+        gap: 10px;
+    }
+
+    .header-links{
+        background-color: green;
+        display: flex;
+        flex-direction: row;
+        justify-content: right;
+        gap: 20px;
+        margin-left: 20px;
+        margin-right: 20px;
+        text-decoration: underline;
+    }
+
+    :global(p, h1, h2, div){
+        font-family:'Courier New', Courier, monospace;
+        color: white;
+        font-size: 20px;
+    }
+
+    header{
+        color: black;
+    }
+</style>
