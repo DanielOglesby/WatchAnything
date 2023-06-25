2227d48d5f5678b7e5ccc2485d201cbff74a7ff5
Daniel Oglesby <dgo4man1@gmail.com>
2023-06-24 20:01:40 -0500

    feat: implement supabase user authentication

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,30 +1,32 @@
 <script lang="ts">
-    import Header from "../../components/Header.svelte";
-    import Footer from "../../components/Footer.svelte";
+	import Header from '../../components/Header.svelte';
+	import Footer from '../../components/Footer.svelte';
 </script>
 
 <section class="about-page">
-    <h1>Welcome to WatchAnything!</h1>
-    <p>WatchAnything is a web application built using Svelte/SvelteKit, TailwindCSS, Vite, and Vercel</p>
+	<h1>Welcome to WatchAnything!</h1>
+	<p>
+		WatchAnything is a web application built using Svelte/SvelteKit, TailwindCSS, Vite, and Vercel
+	</p>
 </section>
 
 <style>
-    .about-page{
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-        background:linear-gradient(250deg, rgb(50,50,255), rgb(150,150,250));
-    }
+	.about-page {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		background: linear-gradient(250deg, rgb(50, 50, 255), rgb(150, 150, 250));
+	}
 
-    :global(p, h1, h2, div){
-        font-family:'Courier New', Courier, monospace;
-        color: white;
-        font-size: 20px;
-    }
+	:global(p, h1, h2, div) {
+		font-family: 'Courier New', Courier, monospace;
+		color: white;
+		font-size: 20px;
+	}
 
-    h1{
-        font-size: 50px;
-    }
+	h1 {
+		font-size: 50px;
+	}
 </style>
